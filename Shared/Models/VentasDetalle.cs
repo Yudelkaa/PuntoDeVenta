1614d76c71632580cd2ee8c044280d49d9a149bc
Yudelkaa <yudelka_guillen@ucne.edu.do>
2024-06-22 10:47:20 -0400

    Pantalla principal

---
+++ Shared/Models/VentasDetalle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Models
+{
+	public class VentasDetalle
+	{
+		[Key]
+		public int DetalleID { get; set; }
+
+		[ForeignKey("Ventas")]
+		public int VentaId { get; set; }
+
+		[ForeignKey("Productos")]
+		public int ProductoId { get; set; }
+
+		[Required(ErrorMessage = "Es requerida la cantidad")]
+		public int Cantidad { get; set; }
+	}
+}
